708c959dd5403a3c9f1a2a3cfc21da0f729380b4
adicomdotir <adicom.ir@gmail.com>
2025-03-19 12:50:16 +0330

    feat: add array/slice sample

---
+++ sum/sum.go
@@ -0,0 +1,20 @@
+package sum
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, item := range numbers {
+		sum += item
+	}
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return sums
+}
